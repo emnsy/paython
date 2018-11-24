28299600bdfe44674b0b0906971cc30854bcaa67
emnsy <43247629+emnsy@users.noreply.github.com>
2018-11-24 17:52:53 +0300

    Add files via upload

---
+++ check_profanity.py
@@ -0,0 +1,19 @@
+﻿import urllib
+
+def read_text():
+    quotes=open("c:\py\movie_quotes.txt")
+    content_of_file=quotes.read()
+    print(content_of_file)
+    quotes.close()
+    check_profanity(content_of_file)
+
+def check_profanity(text_to_check):
+    connection=urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output=connection.read()
+    if 'true' in output:
+        print("يوجد كلمات مخلة داخل الفقرة")
+    else:
+        print("الفقرة خالية من الكلمات المخلة")
+    connection.close()
+
+read_text()    
